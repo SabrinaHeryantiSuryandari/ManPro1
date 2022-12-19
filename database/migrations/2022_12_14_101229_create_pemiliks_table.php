2eb41fa157dd218b2f60205a33f14ac2e8da9c3e
SabrinaHeryantiSuryandari <19081010130@student.upnjatim.ac.id>
2022-12-19 14:05:52 +0700

    first commit

---
+++ database/migrations/2022_12_14_101229_create_pemiliks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pemiliks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tanah_id')->constrained();
+            $table->string('nama');
+            $table->bigInteger('no_ktp');
+            $table->string('ttl');
+            $table->string('pekerjaan');
+            $table->string('alamat');
+            $table->string('no_tlp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pemiliks');
+    }
+};
